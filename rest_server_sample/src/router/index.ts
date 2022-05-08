b90c1e6437a9bffa50c58c815fb9cb1586974e1a
overthestream <njhnjh02@naver.com>
2022-05-08 19:55:02 +0900

    feat/review

---
+++ rest_server_sample/src/router/index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import shopRouter from './shop/index';
+import reviewRouter from './review/index'
+
+const router = express.Router();
+
+router.use('/shop', shopRouter);
+router.use('/review', reviewRouter);
+
+export default router;
