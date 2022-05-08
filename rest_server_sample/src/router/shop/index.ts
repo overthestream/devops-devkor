2ab36ac50bf20034f15e4b891dc3698025f41a33
overthestream <njhnjh02@naver.com>
2022-05-08 19:47:38 +0900

    feat/shop

---
+++ rest_server_sample/src/router/shop/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import { getAllShop, getKeywordById, getShopById, addKeyword, addShop } from './controller';
+
+const router = express.Router();
+
+router.get('/list', getAllShop);
+router.get('/info', getShopById);
+router.post('/add', addShop);
+
+router.get('/keyword/list', getKeywordById);
+router.post('/keyword/add', addKeyword);
+
+export default router;
